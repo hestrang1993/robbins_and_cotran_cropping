0de986b87ccba5428e9c152ac26da2eead87ddcf
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2022-01-10 20:05:42 -0500

    feat(read_pdf_text): Get text from PDF pages
    TODO: Get figure titles and lines only.

--- read_pdf_text.py
+++ read_pdf_text.py
@@ -3,7 +3,7 @@ from variables import pdf_src
 import re
 
 page_text_dict = {}
-search_regex = r"Figure \d([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))? [a-zA-Z]+"
+search_regex = r"Figure \d-\d"
 
 """
 def useRegex(input):
@@ -17,16 +17,25 @@ with open(pdf_src, "rb") as pdf_file_obj:
         page = pdf_reader.getPage(i)
         text = page.extractText()
         # text = re.findall(search_regex, text)
-        text_split = text.split("\n")
+        # text_split = re.split(search_regex, text)
+        text_split = text.split('\n')
+        text = " ".join(text_split)
+        text_split = text.split(".")
+        new_text_split = []
+        for split in text_split:
+            new_text_split.append(f"[{split}]")
         if text is not None:
-            page_text_dict[i] = text_split
+            page_text_dict[i] = new_text_split
         else:
             page_text_dict[i] = [""]
+        print(page.getContents())
 
 
 with open("page_text.txt", "w", encoding="utf-8") as txt_file:
     for page, text in page_text_dict.items():
-        print(str(page))
-        print(f"\t{text}")
+        #print(str(page))
+        #print(f"\t{text}")
         txt_file.writelines(text)
         txt_file.write("\n")
+
+
